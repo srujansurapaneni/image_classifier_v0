95873378e96d8864b3554da2f104b19c590d84df
srujansurapaneni <srujansurapaneni@gmail.com>
2022-03-15 20:09:50 -0400

    removed unwanted args

--- predict.py
+++ predict.py
@@ -60,34 +60,19 @@ def predict(path, model_loaded, top_k):
     return probs, classes
 
 if __name__ == '__main__':
-    print('predict.py, running')
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('arg1')
     parser.add_argument('arg2')
     parser.add_argument('--top_k')
     parser.add_argument('--category_names') 
-    
-    
     args = parser.parse_args()
-    print(args)
-    
-    print('arg1:', args.arg1)
-    print('arg2:', args.arg2)
-    print('top_k:', args.top_k)
-    print('category_names:', args.category_names)
-    
     image_path = args.arg1
-    
     model = tf.keras.models.load_model(args.arg2 ,custom_objects={'KerasLayer':hub.KerasLayer} )
     top_k = args.top_k
-    if top_k is None: 
-        top_k = 5
 
     with open(args.category_names, 'r') as f:
         class_names = json.load(f)
    
-    probs, classes = predict(image_path, model, top_k)
-    
+    probs, classes = predict(image_path, model, top_k) 
     print(probs)
     print(classes)
